2d55bb6ec963398318279412358be401c31e9b8e
mlendvaj <mihael.lendvaj@gmail.com>
2025-08-23 19:33:45 +0200

    Add Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,15 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "chronobuildmaster", appId: "1:271826533750:web:410e7beacfcf8ad6fc4e57", storageBucket: "chronobuildmaster.firebasestorage.app", apiKey: "AIzaSyC87QGYCQBNAn9V39XQf3r-SOFH0XFqsyU", authDomain: "chronobuildmaster.firebaseapp.com", messagingSenderId: "271826533750", measurementId: "G-PLF41B6D4Z" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC87QGYCQBNAn9V39XQf3r-SOFH0XFqsyU",
+    authDomain: "chronobuildmaster.firebaseapp.com",
+    projectId: "chronobuildmaster",
+    storageBucket: "chronobuildmaster.firebasestorage.app",
+    messagingSenderId: "271826533750",
+    appId: "1:271826533750:web:410e7beacfcf8ad6fc4e57"
+  }
+};
