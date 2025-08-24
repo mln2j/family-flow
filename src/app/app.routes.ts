1920dc88189a90a3c44ac945a12db016a0b7249e
mlendvaj <mihael.lendvaj@gmail.com>
2025-08-24 18:47:16 +0200

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { LoginComponent } from './components/auth/login/login';
+import { FamilyComponent } from './components/dashboard/family/family';
+import {RegisterComponent} from './components/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'family', component: FamilyComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: 'family', pathMatch: 'full' },
+  { path: '**', redirectTo: 'family' }
+];
