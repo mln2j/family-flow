eb029be2a208295f07440b2ac1fa5a36900e266f
mlendvaj <mihael.lendvaj@gmail.com>
2025-08-24 19:09:59 +0200

    Update login to redirect to setup if user is not set-up

--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -1,8 +1,9 @@
-import {Component, inject} from '@angular/core';
-import {Auth, signInWithEmailAndPassword} from '@angular/fire/auth';
-import {Router, RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Router , RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +17,7 @@ import {NgIf} from '@angular/common';
   styleUrls: ['./login.sass']
 })
 export class LoginComponent {
+  private firestore = inject(Firestore);
   private auth = inject(Auth);
   private router = inject(Router);
 
@@ -26,10 +28,26 @@ export class LoginComponent {
   async login() {
     this.errorMessage = '';
     try {
-      await signInWithEmailAndPassword(this.auth, this.email, this.password);
-      this.router.navigate(['/family']);
+      const userCredential = await signInWithEmailAndPassword(this.auth, this.email, this.password);
+      const user = userCredential.user;
+
+      const userDocRef = doc(this.firestore, `users/${user.uid}`);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (userDocSnap.exists()) {
+        const userData = userDocSnap.data() as { setupComplete?: boolean };
+
+        if (userData.setupComplete) {
+          this.router.navigate(['']);
+        } else {
+          this.router.navigate(['/set-up']);
+        }
+      } else {
+        this.router.navigate(['/set-up']);
+      }
+
     } catch (error: any) {
-      this.errorMessage = error.message ?? 'Došlo je do pogreške prilikom prijave.';
+      this.errorMessage = error.message || 'Došlo je do pogreške prilikom prijave.';
     }
   }
 }
