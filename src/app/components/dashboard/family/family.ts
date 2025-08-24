35bcf28797661ef28e40299b9ea22b446d37682a
mlendvaj <mihael.lendvaj@gmail.com>
2025-08-24 17:46:27 +0200

    Add family component

---
+++ src/app/components/dashboard/family/family.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FamilyService} from '../../../services/family.service';
+import { Auth, authState } from '@angular/fire/auth';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Family as FamilyModel } from '../../../models/family.model';
+
+@Component({
+  selector: 'app-family',
+  standalone: true,
+  imports: [],
+  templateUrl: './family.html',
+  styleUrls: ['./family.sass'],
+})
+export class FamilyComponent implements OnInit {
+  private familyService = inject(FamilyService);
+  private auth = inject(Auth);
+
+  families$: Observable<FamilyModel[]> = of([]);
+
+  ngOnInit() {
+    this.families$ = authState(this.auth).pipe(
+      switchMap(user => {
+        if (!user) return of([]);
+        return this.familyService.getFamiliesByUserId(user.uid);
+      })
+    );
+  }
+}
